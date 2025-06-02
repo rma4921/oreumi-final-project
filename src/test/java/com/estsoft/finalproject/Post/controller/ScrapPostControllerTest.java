69132233479cb7ee2d7b88c838c670d97a275f21
rma4921 <rma4921@naver.com>
2025-06-02 17:03:05 +0900

    test: ScrapPostController 테스트 코드 추가 - ScrapPostControllerTest 파일 추가

---
+++ src/test/java/com/estsoft/finalproject/Post/controller/ScrapPostControllerTest.java
@@ -0,0 +1,39 @@
+package com.estsoft.finalproject.Post.controller;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+
+import com.estsoft.finalproject.Post.service.ScrapPostService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ScrapPostControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ScrapPostService scrapPostService;
+
+    @Test
+    @DisplayName("스크랩한 기사 저장")
+    void savePost() throws Exception {
+        Long scrapId = 1L;
+
+        mockMvc.perform(post("/api/mypage/{scrapId}", scrapId))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrl("/mypage/scrap/" + scrapId));
+
+        verify(scrapPostService, times(1)).savePost(scrapId);
+    }
+}
