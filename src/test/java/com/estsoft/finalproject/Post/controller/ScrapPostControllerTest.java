b4aca488cc4337e3fdc0b20df58287f9e5118ea7
rma4921 <rma4921@naver.com>
2025-06-04 16:36:52 +0900

    test: 테스트 설정 application-test.properties으로 변경

--- src/test/java/com/estsoft/finalproject/Post/controller/ScrapPostControllerTest.java
+++ src/test/java/com/estsoft/finalproject/Post/controller/ScrapPostControllerTest.java
@@ -13,11 +13,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ScrapPostControllerTest {
     @Autowired
     private MockMvc mockMvc;
--- src/test/java/com/estsoft/finalproject/mypage/service/ScrappedArticleServiceTest.java
+++ src/test/java/com/estsoft/finalproject/mypage/service/ScrappedArticleServiceTest.java
@@ -18,8 +18,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ScrappedArticleServiceTest {
 
     @Autowired
