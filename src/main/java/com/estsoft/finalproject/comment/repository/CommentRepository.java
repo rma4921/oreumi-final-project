40384b5e83c83d977069533d7940418e6a1d542f
BanditKing <plm1414@naver.com>
2025-06-05 16:57:38 +0900

    feat(comment): 댓글 등록, 조회, 수정, 삭제 서비스로직 구현

---
+++ src/main/java/com/estsoft/finalproject/comment/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.estsoft.finalproject.comment.repository;
+
+import com.estsoft.finalproject.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
+    List<Comment> findByUserId(Long userId);
+}
---
+++ src/main/java/com/estsoft/finalproject/comment/service/CommentService.java
@@ -0,0 +1,78 @@
+package com.estsoft.finalproject.comment.service;
+
+import com.estsoft.finalproject.comment.repository.UserRepository;
+import com.estsoft.finalproject.comment.entity.User;
+
+import com.estsoft.finalproject.comment.repository.CommentRepository;
+import com.estsoft.finalproject.comment.dto.CommentRequest;
+import com.estsoft.finalproject.comment.entity.Comment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+
+    public Comment save(CommentRequest request) {
+        validateContent(request.getContent());
+
+        LocalDateTime now = LocalDateTime.now();
+
+        String nickname = userRepository.findById(request.getUserId())
+                .map(User::getNickname)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저를 못 찾았습니다."));
+
+        Comment comment = Comment.builder()
+                .postId(request.getPostId())
+                .userId(request.getUserId())
+                .nickname(nickname)
+                .content(request.getContent())
+                .createTime(now)
+                .updateTime(now)
+                .build();
+
+        return commentRepository.save(comment);
+    }
+
+
+    public List<Comment> findByPostId(Long postId) {
+        return commentRepository.findByPostId(postId);
+    }
+
+    @Transactional
+    public Comment updateComment(Long commentId, CommentRequest request) {
+        validateContent(request.getContent());
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
+
+        comment.setContent(request.getContent());
+
+        return comment;
+    }
+
+    @Transactional
+    public void deleteComment(Long id) {
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
+        commentRepository.delete(comment);
+    }
+
+
+    private void validateContent(String content) {
+        if (content == null || content.trim().isEmpty()) {
+            throw new IllegalArgumentException("빈 공백은 등록할 수 없어요");
+        }
+    }
+
+    public List<Comment> findByUserId(Long userId) {
+        return commentRepository.findByUserId(userId);
+    }
+}
