8d3b1efb2ddf95620e0eba304ed0d61d28a8fa90
BanditKing <plm1414@naver.com>
2025-06-05 17:00:55 +0900

    feat: 전역 예외 처리 핸들러 추가

---
+++ src/main/java/com/estsoft/finalproject/config/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.estsoft.finalproject.config;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+}
