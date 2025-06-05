174cf57ceafc9a433565bef1c180d70b40331f9c
BanditKing <plm1414@naver.com>
2025-06-05 17:10:37 +0900

    feat(config): JPA Auditing 활성화를 위한 어노테이션 추가

--- src/main/java/com/estsoft/finalproject/FinalProjectApplication.java
+++ src/main/java/com/estsoft/finalproject/FinalProjectApplication.java
@@ -2,10 +2,11 @@ package com.estsoft.finalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FinalProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FinalProjectApplication.class, args);
     }
