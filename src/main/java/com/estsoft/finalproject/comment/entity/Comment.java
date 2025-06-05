45ea57745fbd5522a69a1786169ebca0199d9362
BanditKing <plm1414@naver.com>
2025-06-05 16:56:58 +0900

    feat(comment): 댓글 엔티티 생성 및 JPA 매핑

---
+++ src/main/java/com/estsoft/finalproject/comment/entity/Comment.java
@@ -0,0 +1,46 @@
+package com.estsoft.finalproject.comment.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "post_id", nullable = false)
+    private Long postId;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    @Column(name = "comment_date", nullable = false, updatable = false)
+    private LocalDateTime createTime;
+
+    @LastModifiedDate
+    @Column(name = "updated_date")
+    private LocalDateTime updateTime;
+
+}
