c03b7ecd2ed7f5fe7641ef17ad8fc0641e6907d8
BanditKing <plm1414@naver.com>
2025-06-14 15:32:49 +0900

    fix: 댓글 조회 시 사용자 ID 기반 쿼리 오류 수정

--- src/main/java/com/estsoft/finalproject/comment/repository/CommentRepository.java
+++ src/main/java/com/estsoft/finalproject/comment/repository/CommentRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPostId(Long postId);
-    List<Comment> findByUserId(Long userId);
+    List<Comment> findByUser_UserId(Long userId);
 }
