b8c90bf296cc7e2b0b0723d7f3617b3ecd1406c1
BanditKing <plm1414@naver.com>
2025-06-16 09:27:51 +0900

    feat: 게시글 상세 페이지에 로그인 사용자 정보 및 postId 전달 기능 추가

--- src/main/java/com/estsoft/finalproject/comment/controller/PageController.java
+++ src/main/java/com/estsoft/finalproject/comment/controller/PageController.java
@@ -1,13 +1,25 @@
 package com.estsoft.finalproject.comment.controller;
 
+import com.estsoft.finalproject.user.dto.CustomUsersDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PageController {
 
     @GetMapping("/posts/{postId}")
-    public String showCommentPage() {
+    public String showCommentPage(@PathVariable Long postId,
+                                  Model model,
+                                  @AuthenticationPrincipal CustomUsersDetails userDetails) {
+        model.addAttribute("postId", postId);
+
+        if (userDetails != null) {
+            model.addAttribute("user", userDetails.getUser());
+            model.addAttribute("userId", userDetails.getUser().getUserId());
+        }
+
         return "postDetail";
     }
 }
